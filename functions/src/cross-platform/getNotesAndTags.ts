12de2683b875547b84eba529919c5d37c5eec193
Andrew Dimmer <43498863+andrewdimmer@users.noreply.github.com>
2020-07-04 16:29:23 -0400

    feat(functions): Can Now Get a Limited Number of Notes (#11)
    Note: Actually committed the correct file this time...

--- functions/src/cross-platform/getNotesAndTags.ts
+++ functions/src/cross-platform/getNotesAndTags.ts
@@ -5,10 +5,33 @@ import { Note, Tag } from "../@types";
 /**
  * Gets the notes with the given tags; if no tags are given, it returns all notes.
  *
- * @param tags An array of the tagIds that the note must have
- * @returns An array of the notes with the given tags
+ * @param tags An array of the tagIds that the note must have.
+ * @returns An array of the notes with the given tags.
  */
 export const getNotes = (tags: string[]): Promise<Note[]> => {
+  return notesRef
+    .where("tags", "array-contains-any", tags)
+    .orderBy("updated", "desc")
+    .get()
+    .then((queryResults) => {
+      return queryResults.docs.map((snapshot) => {
+        return snapshot.data() as Note;
+      });
+    })
+    .catch(logReturnEmptyArray);
+};
+
+/**
+ * Gets a limited number of notes with the given tags; if no tags are given, it returns all notes.
+ *
+ * @param tags An array of the tagIds that the note must have.
+ * @param limit The number of notes to get.
+ * @returns An array of the notes with the given tags.
+ */
+export const getNotesLimited = (
+  tags: string[],
+  limit: number
+): Promise<Note[]> => {
   return notesRef
     .where("tags", "array-contains-any", tags)
     .orderBy("updated", "desc")
