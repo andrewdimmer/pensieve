9c04f048439d16429a7eec577e6195e257187f15
Andrew Dimmer <contactandrewd@gmail.com>
2020-07-05 01:13:20 -0400

    feat(functions): Exposed Flutter Endpoints to Delete and Edit Notes

---
+++ functions/src/flutter/manageNotesFlutter.ts
@@ -0,0 +1,50 @@
+import * as functions from "firebase-functions";
+import { getNotes } from "../cross-platform/getNotesAndTags";
+import {
+  deleteNote,
+  editNoteCompleteness,
+} from "../cross-platform/manageNotes";
+
+export const getNotesFlutter = functions.https.onRequest(
+  (request, response) => {
+    response.setHeader("Access-Control-Allow-Origin", "*"); // TODO: Make more secure later!
+    const requestBody = JSON.parse(request.body) as {
+      complete: boolean;
+      tags: string[];
+    };
+    getNotes(requestBody.complete, requestBody.tags ? requestBody.tags : [])
+      .then((notes) => response.status(200).send({ notes }))
+      .catch((err) => {
+        console.log(err);
+        response.status(500).send("Unable to get notes.");
+      });
+  }
+);
+
+export const editNoteCompletenessFlutter = functions.https.onRequest(
+  (request, response) => {
+    response.setHeader("Access-Control-Allow-Origin", "*"); // TODO: Make more secure later!
+    const requestBody = JSON.parse(request.body) as {
+      noteId: string;
+      complete: boolean;
+    };
+    editNoteCompleteness(requestBody.noteId, requestBody.complete)
+      .then((status) => response.status(200).send(status))
+      .catch((err) => {
+        console.log(err);
+        response.status(500).send("Unable to update completeness.");
+      });
+  }
+);
+
+export const deleteNoteFlutter = functions.https.onRequest(
+  (request, response) => {
+    response.setHeader("Access-Control-Allow-Origin", "*"); // TODO: Make more secure later!
+    deleteNote(request.body)
+      .then((status) => response.status(200).send(status))
+      .catch((err) => {
+        console.log(err);
+        response.status(500).send("Unable to delete note.");
+      });
+  }
+);
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,18 @@
 import { alexaSkill } from "./alexa/handlers";
 import { tagLastNoteHelperTest } from "./alexa/tagLastNoteHelper";
-import { getNotesFlutter } from "./flutter/getNotesFlutter";
+import {
+  deleteNoteFlutter,
+  editNoteCompletenessFlutter,
+  getNotesFlutter,
+} from "./flutter/manageNotesFlutter";
 
 // Alexa Skill
 export const alexa_handlers = alexaSkill;
 
 // Flutter App
 export const get_notes_flutter = getNotesFlutter;
+export const edit_note_completeness_flutter = editNoteCompletenessFlutter;
+export const delete_note_flutter = deleteNoteFlutter;
 
 // Test
 export const tag_last_note_helper_test = tagLastNoteHelperTest;
