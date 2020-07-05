7d4ca800c047dd6d97dc67c54e3127192772a2c5
Andrew Dimmer <43498863+andrewdimmer@users.noreply.github.com>
2020-07-04 22:36:02 -0400

    feat(functions): Added Helpers to Tag the Latest Note (#14)
    * fix(functions): Fixed No Return Error for Note and Tag Management
    
    * feat(fuctions): getTags Can Now Get Tags With a Given TagName
    
    * feat(alexa): Added a Helper Function to Tag the Latest Note

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,12 @@
 import { alexaSkill } from "./alexa/handlers";
+import { tagLastNoteHelperTest } from "./alexa/tagLastNoteHelper";
 import { getNotesFlutter } from "./flutter/getNotesFlutter";
 
+// Alexa Skill
 export const alexa_handlers = alexaSkill;
 
+// Flutter App
 export const get_notes_flutter = getNotesFlutter;
+
+// Test
+export const tag_last_note_helper_test = tagLastNoteHelperTest;
