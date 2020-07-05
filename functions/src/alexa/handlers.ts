50b05395877084c276d978fb6c3dc84086a845c7
Andrew Dimmer <43498863+andrewdimmer@users.noreply.github.com>
2020-07-05 02:59:43 -0400

    feat(alexa): Added New Handlers to Add Tags and Get Notes (#17)
    * feat(alexa): Partial handlers
    
    * fix(alexa): Added Error Handling and Refactored Equality Checks
    
    * fix(alexa): Re-added Create Note to Alexa Skills
    
    * fix(alexa): Add Tag to Last Note Helper Now Returns the Note Contents
    
    * feat(alexa): almost complete handlers
    
    * fix(alexa): Fixed Duplicate Tag Bug; Now Reads Note After Tagging
    
    Co-authored-by: Nathan Dimmer <nathandimmer@gmail.com>

--- functions/src/alexa/handlers.ts
+++ functions/src/alexa/handlers.ts
@@ -1,25 +1,57 @@
 import * as functions from "firebase-functions";
 import { createNote } from "../cross-platform/manageNotes";
+import { getNotes } from "../cross-platform/getNotesAndTags";
+import { Note } from "../@types";
+import { tagLastNoteHelper } from "./tagLastNoteHelper";
 
 export const alexaSkill = functions.https.onRequest((request, response) => {
   const type = JSON.stringify(request.body.request.type);
   let name = "";
+  let note = "";
+  let tag = "";
+  let number = 0;
 
   try {
     name = JSON.stringify(request.body.request.intent.name);
+    if (name.indexOf("AddNote") >= 0) {
+      note = JSON.stringify(request.body.request.intent.slots["note"].value);
+    } else if (name.indexOf("AddTag") >= 0) {
+      tag = JSON.stringify(request.body.request.intent.slots["tag"].value);
+    } else if (name.indexOf("GetNotes") >= 0) {
+      number = parseInt(
+        JSON.stringify(request.body.request.intent.slots["number"].value)
+      );
+    } else if (name.indexOf("GetTaggedNotes") >= 0) {
+      // number = parseInt(
+      //   JSON.stringify(request.body.request.intent.slots["number"].value)
+      // );
+      number = 5;
+      tag = JSON.stringify(request.body.request.intent.slots["tag"].value);
+    }
   } catch (e) {}
 
-  let note = "";
-
-  if (type == '"IntentRequest"') {
-    note = JSON.stringify(request.body.request.intent.slots["note"].value);
-  }
-  const result = getAlexaResponse(type, name, note);
-  response.send(result);
+  getAlexaResponse(type, name, note, tag, number)
+    .then((results) => {
+      response.status(200).send(results);
+    })
+    .catch((err) => {
+      console.log(err);
+      response
+        .status(500)
+        .send(
+          "Sorry, but we're unable to fulfill your request at this time. Please try again later."
+        );
+    });
 });
 
-const getAlexaResponse = (type: string, name: string, note: string) => {
-  var AlexaDefaultAnswer = {
+const getAlexaResponse = async (
+  type: string,
+  name: string,
+  note: string,
+  tag: string,
+  number: number
+) => {
+  const AlexaDefaultAnswer = {
     version: "1.0",
     response: {
       outputSpeech: {
@@ -39,14 +71,122 @@ const getAlexaResponse = (type: string, name: string, note: string) => {
     sessionAttributes: {},
   };
 
-  if (type == '"LaunchRequest"') {
+  if (type.indexOf("LaunchRequest") >= 0) {
     return AlexaDefaultAnswer;
-  } else if (type == '"IntentRequest"' && name == '"AddNote"') {
-    createNote(note);
+  } else if (
+    type.indexOf("IntentRequest") >= 0 &&
+    name.indexOf("AddNote") >= 0
+  ) {
+    return createNote(note)
+      .then((noteId) => {
+        if (noteId) {
+          AlexaDefaultAnswer.response.outputSpeech.ssml =
+            "<speak>" + "Note added: " + note + "</speak>";
+          AlexaDefaultAnswer.response.card.content = "Note added: " + note;
+        } else {
+          AlexaDefaultAnswer.response.outputSpeech.ssml =
+            "<speak>There was an error adding your note. Please try again later.</speak>";
+          AlexaDefaultAnswer.response.card.content =
+            "There was an error adding your note. Please try again later.";
+        }
+        return AlexaDefaultAnswer;
+      })
+      .catch((err) => {
+        console.log(err);
+        AlexaDefaultAnswer.response.outputSpeech.ssml =
+          "<speak>There was an error adding your note. Please try again later.</speak>";
+        AlexaDefaultAnswer.response.card.content =
+          "There was an error adding your note. Please try again later.";
+        return AlexaDefaultAnswer;
+      });
+  } else if (
+    type.indexOf("IntentRequest") >= 0 &&
+    name.indexOf("AddTag") >= 0
+  ) {
+    return tagLastNoteHelper(tag)
+      .then((noteContent) => {
+        if (noteContent) {
+          AlexaDefaultAnswer.response.outputSpeech.ssml =
+            "<speak>" +
+            "Added tag: " +
+            tag +
+            "to the note " +
+            noteContent +
+            "</speak>";
+          AlexaDefaultAnswer.response.card.content =
+            "Added tag: " + tag + " to the note" + noteContent;
+          return AlexaDefaultAnswer;
+        } else {
+          AlexaDefaultAnswer.response.outputSpeech.ssml =
+            "<speak>There was an error adding your note. Please try again later.</speak>";
+          AlexaDefaultAnswer.response.card.content =
+            "There was an error adding your note. Please try again later.";
+        }
+        return AlexaDefaultAnswer;
+      })
+      .catch((err) => {
+        console.log(err);
+        AlexaDefaultAnswer.response.outputSpeech.ssml =
+          "<speak>There was an error adding your note. Please try again later.</speak>";
+        AlexaDefaultAnswer.response.card.content =
+          "There was an error adding your note. Please try again later.";
+        return AlexaDefaultAnswer;
+      });
+  } else if (
+    type.indexOf("IntentRequest") >= 0 &&
+    name.indexOf("GetNotes") >= 0
+  ) {
     AlexaDefaultAnswer.response.outputSpeech.ssml =
-      "<speak>" + "Note added: " + note + "</speak>";
-    AlexaDefaultAnswer.response.card.content = "Note added: " + note;
-    return AlexaDefaultAnswer;
+      "<speak>" + "Getting your last " + number + " notes.";
+    AlexaDefaultAnswer.response.card.content =
+      "Getting your last " + number + " notes. \n";
+    return getNotes(false, [], number)
+      .then((notes) => {
+        for (let i = 0; i < notes.length; i++) {
+          AlexaDefaultAnswer.response.outputSpeech.ssml +=
+            "<speak>" + "Note " + i + ": " + notes[i].content;
+          AlexaDefaultAnswer.response.card.content +=
+            "Note " + i + ": " + notes[i].content + "\n";
+        }
+        AlexaDefaultAnswer.response.outputSpeech.ssml += "</speak>";
+        return AlexaDefaultAnswer;
+      })
+      .catch((err) => {
+        console.log(err);
+        AlexaDefaultAnswer.response.outputSpeech.ssml =
+          "<speak>There was an error completing your request. Please try again later.</speak>";
+        AlexaDefaultAnswer.response.card.content =
+          "There was an error completing your request. Please try again later.";
+        return AlexaDefaultAnswer;
+      });
+  } else if (
+    type.indexOf("IntentRequest") >= 0 &&
+    name.indexOf("GetTaggedNotes") >= 0
+  ) {
+    AlexaDefaultAnswer.response.outputSpeech.ssml =
+      "<speak>" + "Getting your last " + number + " notes." + "</speak>";
+    AlexaDefaultAnswer.response.card.content =
+      "Getting your last " + number + " notes. \n";
+    let notes: Note[] = [];
+    return getNotes(false, [tag], number)
+      .then((value) => {
+        notes = value;
+        for (let i = 0; i < notes.length; i++) {
+          AlexaDefaultAnswer.response.outputSpeech.ssml +=
+            "<speak>" + "Note " + i + ": " + notes[i].content + "</speak>";
+          AlexaDefaultAnswer.response.card.content +=
+            "Note " + i + ": " + notes[i].content + "\n";
+        }
+        return AlexaDefaultAnswer;
+      })
+      .catch((err) => {
+        console.log(err);
+        AlexaDefaultAnswer.response.outputSpeech.ssml =
+          "<speak>There was an error completing your request. Please try again later.</speak>";
+        AlexaDefaultAnswer.response.card.content =
+          "There was an error completing your request. Please try again later.";
+        return AlexaDefaultAnswer;
+      });
   } else {
     return AlexaDefaultAnswer;
   }
--- functions/src/alexa/tagLastNoteHelper.ts
+++ functions/src/alexa/tagLastNoteHelper.ts
@@ -2,8 +2,14 @@ import * as functions from "firebase-functions";
 import { getNotes, getTags } from "../cross-platform/getNotesAndTags";
 import { createTag } from "../cross-platform/manageTags";
 import { addTag } from "../cross-platform/tagUntag";
-import { logReturnFalse } from "../helpers/errorHelpers";
+import { logReturnEmptyString } from "../helpers/errorHelpers";
 
+/**
+ * Addes the given tag to the last updated note.
+ *
+ * @param tagNameRaw The raw name of the tag before normalizing it.
+ * @returns The contents of the note that was updated if the tag was added successfully; otherwise an empty string.
+ */
 export const tagLastNoteHelper = (tagNameRaw: string) => {
   const tagName = tagNameRaw
     .toLowerCase()
@@ -21,28 +27,28 @@ export const tagLastNoteHelper = (tagNameRaw: string) => {
             if (tags.length > 0) {
               // Tag already exists, add the tag to the note
               return addTag(notes[0].noteId, tags[0].tagId)
-                .then((status) => status)
-                .catch(logReturnFalse);
+                .then((status) => (status ? notes[0].content : ""))
+                .catch(logReturnEmptyString);
             } else {
               // Tag does not exists, create a new tag
               return createTag(tagName)
                 .then((tagId) => {
                   // Add the new tag to the note
                   return addTag(notes[0].noteId, tagId)
-                    .then((status) => status)
-                    .catch(logReturnFalse);
+                    .then((status) => (status ? notes[0].content : ""))
+                    .catch(logReturnEmptyString);
                 })
-                .catch(logReturnFalse);
+                .catch(logReturnEmptyString);
             }
           })
-          .catch(logReturnFalse);
+          .catch(logReturnEmptyString);
       } else {
         // No note exists to add a tag to
         console.log("There are no open notes to tag.");
-        return false;
+        return "";
       }
     })
-    .catch(logReturnFalse);
+    .catch(logReturnEmptyString);
 };
 
 export const tagLastNoteHelperTest = functions.https.onRequest(
