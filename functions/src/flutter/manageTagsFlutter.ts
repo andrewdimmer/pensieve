8c71be4c276a1bf9e92b5b45cfa51ac24be4d9a6
Andrew Dimmer <contactandrewd@gmail.com>
2020-07-05 05:20:41 -0400

    feat(functions): Exposed Flutter Endpoints to Add and Remove Tags

--- functions/src/flutter/manageTagsFlutter.ts
+++ functions/src/flutter/manageTagsFlutter.ts
@@ -1,5 +1,6 @@
 import * as functions from "firebase-functions";
 import { getTags } from "../cross-platform/getNotesAndTags";
+import { addTag, removeTag } from "../cross-platform/tagUntag";
 
 export const getTagsFlutter = functions.https.onRequest((request, response) => {
   response.setHeader("Access-Control-Allow-Origin", "*"); // TODO: Make more secure later!
@@ -10,3 +11,33 @@ export const getTagsFlutter = functions.https.onRequest((request, response) => {
       response.status(500).send("Unable to get tags.");
     });
 });
+
+export const addTagFlutter = functions.https.onRequest((request, response) => {
+  response.setHeader("Access-Control-Allow-Origin", "*"); // TODO: Make more secure later!
+  const requestBody = JSON.parse(request.body) as {
+    noteId: string;
+    tagId: string;
+  };
+  addTag(requestBody.noteId, requestBody.tagId)
+    .then((status) => response.status(200).send(status))
+    .catch((err) => {
+      console.log(err);
+      response.status(500).send("Unable to update completeness.");
+    });
+});
+
+export const removeTagFlutter = functions.https.onRequest(
+  (request, response) => {
+    response.setHeader("Access-Control-Allow-Origin", "*"); // TODO: Make more secure later!
+    const requestBody = JSON.parse(request.body) as {
+      noteId: string;
+      tagId: string;
+    };
+    removeTag(requestBody.noteId, requestBody.tagId)
+      .then((status) => response.status(200).send(status))
+      .catch((err) => {
+        console.log(err);
+        response.status(500).send("Unable to update completeness.");
+      });
+  }
+);
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,7 +6,11 @@ import {
   editNoteOrderFlutter,
   getNotesFlutter,
 } from "./flutter/manageNotesFlutter";
-import { getTagsFlutter } from "./flutter/manageTagsFlutter";
+import {
+  addTagFlutter,
+  getTagsFlutter,
+  removeTagFlutter,
+} from "./flutter/manageTagsFlutter";
 
 // Alexa Skill
 export const alexa_handlers = alexaSkill;
@@ -17,6 +21,8 @@ export const get_tags_flutter = getTagsFlutter;
 export const edit_note_completeness_flutter = editNoteCompletenessFlutter;
 export const edit_note_order_flutter = editNoteOrderFlutter;
 export const delete_note_flutter = deleteNoteFlutter;
+export const add_tag_flutter = addTagFlutter;
+export const remove_tag_flutter = removeTagFlutter;
 
 // Test
 export const tag_last_note_helper_test = tagLastNoteHelperTest;
